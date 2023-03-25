a57a24a3d441d98df15cdf060bd9bd8cc70b1c11
XiangGameCAO <1323943635@qq.com>
2023-03-25 11:29:31 +0800

    header and footer static pages

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+//引入路由
+import Home from '@/pages/Home'
+import Search from '@/pages/Search'
+import Login from '@/pages/Login'
+import Register from '@/pages/Register'
+export default new VueRouter({
+    routes: [
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path: '/search',
+            component: Search
+        }, {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/register',
+            component: Register
+        },
+        {
+            path: '*',
+            redirect: '/home'
+        }
+    ]
+})
